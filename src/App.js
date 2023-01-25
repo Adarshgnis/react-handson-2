5cfcb1e803db604aba989c9bee63a44eabf58f8d
- <=>
2023-01-25 17:59:52 +0530

    Handson 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
+export default class App extends Component {
+
+  state = {
+      name: "",
+      department: "",
+      rating: "",
+      data: []
+  }
+
+  handleChange = (e) => {
+      this.setState({[e.target.name] : e.target.value})
+            
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const saveData = {
+      name: this.state.name,
+      department: this.state.department,
+      rating: this.state.rating
+    }
+    const arr = this.state.data
+    arr.push(saveData)
+    this.setState({data : arr})
+
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='heading'>Employee Feedback Form</h1>
+      <form id="my-form">
+        <label>Name :</label> <input type='text' name='name' value={this.state.name} onChange={this.handleChange} /> <br />
+        <label>Department :</label> <input type='text' name='department' value={this.state.department} onChange={this.handleChange} /> <br />
+        <label>Rating :</label> <input type='number' name='rating' value={this.state.rating} onChange={this.handleChange} /> <br />
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+      </form>
+
+      <div className='section'>
+        {this.state.data.map((value,index) => {
+          return(
+            <div key={index} className="card">
+              <p key={index}>Name: {value.name} | Department: {value.department} | Rating: {value.rating} </p>
+            </div>
+          )
+        })}
+      </div>
+      
     </div>
-  );
+  )
+}
 }
 
-export default App;
+
+
